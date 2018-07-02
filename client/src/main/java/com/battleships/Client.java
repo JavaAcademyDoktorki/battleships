165ed4a6d94976b7b30eb551ac52a67417f9cb36
Hoymm <damian.andrzej.muca@gmail.com>
2018-07-02 16:35:15 +0200

    Refactor Client class

--- client/src/main/java/com/battleships/Client.java
+++ client/src/main/java/com/battleships/Client.java
@@ -6,23 +6,67 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class Client extends Application {
     private WindowScalling windowScalling;
+    private static final Logger logger = LogManager.getLogger(Client.class.getName());
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        windowScalling = new WindowScalling();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("start_window/start_window.fxml"));
-        Parent root = loader.load();
+    public void start(Stage primaryStage) {
+        enableAppTitleTranslation(primaryStage);
+        setSceneRunAppIfViewLoaded(primaryStage);
+    }
+
+    private void enableAppTitleTranslation(Stage primaryStage) {
         primaryStage.titleProperty().bind(Translator.createStringBinding("battleships_title"));
+    }
+
+    private void setSceneRunAppIfViewLoaded(Stage primaryStage) {
+        Optional<Parent> rootFxml = tryToGetRootFxml();
+        if (rootFxml.isPresent()) {
+            setScalingScene(primaryStage, rootFxml.get());
+            primaryStage.show();
+        }
+        else{
+            logger.error("Nie mogłem załadować głownego widoku fxml, nie odpaliłem aplikacji.");
+        }
+    }
+
+    private Optional<Parent> tryToGetRootFxml() {
+        String mainFxmlPath = "start_window/start_window.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(mainFxmlPath));
+        return tryToLoadRoot(mainFxmlPath, loader);
+    }
+
+    private Optional<Parent> tryToLoadRoot(String mainFxmlPath, FXMLLoader loader) {
+        Optional <Parent> root = Optional.empty();
+        try {
+            root = Optional.of(loader.load());
+        } catch (IOException e) {
+            logger.error(String.format("Nie mogłem załadować głównego pliku xml aplikacji ze ścieżki %s", mainFxmlPath), e.getMessage());
+        }
+        finally {
+            return root;
+        }
+    }
+
+    private void setScalingScene(Stage primaryStage, Parent root) {
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
-        primaryStage.show();
+        enableScaling(root, scene);
+    }
+
+    private void enableScaling(Parent root, Scene scene) {
+        windowScalling = new WindowScalling();
         windowScalling.enableFor(scene, root);
     }
 }
