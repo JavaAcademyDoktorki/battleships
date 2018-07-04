8f769450d1dcadb822859151a40e7303c59467f0
Hoymm <damian.andrzej.muca@gmail.com>
2018-07-04 14:30:50 +0200

    Fix client application (make class public)

--- client/src/main/java/com/battleships/start_window/data_insertion/SettingsTextData.java
+++ client/src/main/java/com/battleships/start_window/data_insertion/SettingsTextData.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
 
-class SettingsTextData {
+public class SettingsTextData {
     @FXML
     private Button connectToServerButton;
     @FXML
