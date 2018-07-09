43dfd31ce9c3a2738cd1843050095a19b97a4490
agaped <aga.pedlewska@gmail.com>
2018-07-09 15:02:33 +0200

    add javadoc for common application

--- common/src/main/java/com/battleships/Command.java
+++ common/src/main/java/com/battleships/Command.java
@@ -1,5 +1,8 @@
 package com.battleships;
 
+/**
+ * Set of commands for communication between client and server
+ */
 public enum Command {
     START_PLAYING, STOP_PLAYING, SET_NAME;
 }
--- common/src/main/java/com/battleships/CommunicatingProtocol.java
+++ common/src/main/java/com/battleships/CommunicatingProtocol.java
@@ -1,7 +1,15 @@
 package com.battleships;
 
+/**
+ * Sets communication protocol between client and serwer
+ */
 public class CommunicatingProtocol {
 
+    /**
+     * Provides a separator used in communication protocol
+     *
+     * @return - returns a separator in a String format
+     */
     public static String getSeparator(){
         return ":";
     }
