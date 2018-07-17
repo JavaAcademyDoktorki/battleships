923b3ed024a5c032668f3c60b2546dfda5d89ca0
Krzysztof Dzioba <krzysztof.dzioba.93@gmail.com>
2018-07-17 17:01:19 +0200

    Add buttons colouring

--- client/src/main/java/com/battleships/game_window/GameWindowController.java
+++ client/src/main/java/com/battleships/game_window/GameWindowController.java
@@ -10,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.util.Random;
+
 public class GameWindowController {
 
     @FXML
@@ -34,9 +36,22 @@ public class GameWindowController {
             @Override
             public void handle(ActionEvent event) {
                 shot(event);
+                colourButton(button, i, j);
             }
         });
         board.add(button, i, j);
+    }
+
+    private void colourButton(Button button, int i, int j) {
+        if(shipWasHit(i, j))
+            button.setStyle("-fx-background-color: #AA3939");
+        else
+            button.setStyle("-fx-background-color: #00ff00");
+    }
+
+    private boolean shipWasHit(int i, int j) {
+        int i1 = new Random().nextInt(100) % 2;
+        return i1 == 0; // TODO zmienic to :)
     }
 
     private void shot(ActionEvent event) {
