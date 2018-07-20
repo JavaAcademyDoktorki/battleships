03e291df884c1877436c0371278fb570a64d6c92
agaped <aga.pedlewska@gmail.com>
2018-07-20 07:06:17 +0200

    SHOT_EVALUATION commend mark as TODO, for now removed from execution on server

--- client/src/main/java/com/battleships/start_window/connection/Commands/ShotMessageReceived.java
+++ client/src/main/java/com/battleships/start_window/connection/Commands/ShotMessageReceived.java
@@ -1,7 +1,5 @@
 package com.battleships.start_window.connection.Commands;
 
-import com.battleships.commands.CommandType;
-import com.battleships.commands.Message;
 import com.battleships.commands.Values.Shot;
 import com.battleships.start_window.connection.Connection;
 import javafx.application.Platform;
@@ -15,12 +13,13 @@ public class ShotMessageReceived<V> extends AbstractServerCommand {
     public void execute() {
         Shot shot = (Shot) value;
         //TODO if hit, then receiver is set inactive and unready, assume always missed to show that turn changes
-        boolean missed=true;
+        boolean missed = true;
         Platform.runLater(() -> Connection.INSTANCE.setPlayerActive(missed));
         Platform.runLater(() -> Connection.INSTANCE.setPlayerReady(missed));
 
-        System.out.println("received shot: " + shot+ "-MISSED"); // TODO usunac sout, dopracować metodę
+        System.out.println("received shot: " + shot + "-MISSED"); // TODO usunac sout
 
-        Connection.INSTANCE.sendToServer(new Message<>(CommandType.SHOT_EVALUATION,false));
+        //TODO after validation send the result to the opponent to block/unblock his opponent board
+        //TODO Connection.INSTANCE.sendToServer(new Message<>(CommandType.SHOT_EVALUATION,false));
     }
 }
