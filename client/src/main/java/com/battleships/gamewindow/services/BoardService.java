ef1134a714f6f1341d51932c0f157e4d973f4917
Hoymm <damian.andrzej.muca@gmail.com>
2018-07-24 12:00:11 +0200

    Uncomment code - fix for last commit

--- client/src/main/java/com/battleships/gamewindow/services/BoardService.java
+++ client/src/main/java/com/battleships/gamewindow/services/BoardService.java
@@ -7,9 +7,12 @@ import com.battleships.gamewindow.board.PlayerBoard;
 import com.battleships.gamewindow.board.fieldStates.BoardField;
 import com.battleships.gamewindow.board.fieldStates.EmptyField;
 import com.battleships.gamewindow.board.fieldStates.SeaField;
+import com.battleships.models.Events;
 import com.battleships.models.board.BoardGridPanes;
 import com.battleships.models.board.Coordinate;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 
@@ -30,35 +33,35 @@ public class BoardService {
         playerBoard.placeFleetRandomly(gridPaneForBoard);
     }
 
-    public void initBoards(BoardGridPanes boardGridPanes) {
+    public void initBoards(BoardGridPanes boardGridPanes, Events events) {
         for (int row = 1; row <= 10; row++) {
             for (int col = 1; col <= 10; col++) {
                 Coordinate coordinate = Coordinate.fromIntCoords(row, col);
                 // TODO 30/07/18 damian - is events.getPlaceShipEvent() needed ?
-                addFieldToPlayerBoard(coordinate, boardGridPanes.playerGridPane()/*, events.getPlaceShipEvent()*/);
-                addFieldToOpponentBoard(coordinate, boardGridPanes.opponentGridPane()/*, events.getShotEvent()*/);
+                addFieldToPlayerBoard(coordinate, boardGridPanes.playerGridPane(), events.getPlaceShipEvent());
+                addFieldToOpponentBoard(coordinate, boardGridPanes.opponentGridPane(), events.getShotEvent());
             }
         }
     }
 
     // TODO 30/07/18 damian -  Refactor that method (too many args)
-    private void addFieldToPlayerBoard(Coordinate coordinate, GridPane boardGridPane/*, EventHandler<ActionEvent> event*/) {
+    private void addFieldToPlayerBoard(Coordinate coordinate, GridPane boardGridPane, EventHandler<ActionEvent> event) {
         BoardField boardField = new SeaField();
         boardField.setDisable(true);
         playerBoard.addNewField(coordinate, boardField);
 
         boardField.setId(coordinate.getRow() + " " + coordinate.getRow()); // TODO 30/07/18 damian - this should not be ID, it should be Coordinates class
-//        boardField.setOnAction(event);
+        boardField.setOnAction(event);
         boardGridPane.add(boardField, coordinate.getColumn(), coordinate.getRow());
     }
 
-    private void addFieldToOpponentBoard(Coordinate coordinate, GridPane boardGridPane/*, EventHandler<ActionEvent> event*/) {
+    private void addFieldToOpponentBoard(Coordinate coordinate, GridPane boardGridPane, EventHandler<ActionEvent> event) {
         BoardField boardField = new EmptyField();
         boardField.disableProperty().bind(Connection.INSTANCE.playerReadyProperty().not());
         opponentBoard.addNewField(coordinate, boardField);
 
         boardField.setId(coordinate.getRow() + " " + coordinate.getRow()); // TODO 30/07/18 damian - this should not be ID, it should be Coordinates class
-//        boardField.setOnAction(event);
+        boardField.setOnAction(event);
         boardGridPane.add(boardField, coordinate.getColumn(), coordinate.getRow());
     }
 
