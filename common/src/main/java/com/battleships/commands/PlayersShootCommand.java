a4ebbcefeef9ce8566c4be40b50f94a1308f9258
agaped <aga.pedlewska@gmail.com>
2018-07-25 10:42:50 +0200

    project builds now

--- common/src/main/java/com/battleships/commands/PlayersShootCommand.java
+++ common/src/main/java/com/battleships/commands/PlayersShootCommand.java
@@ -1,6 +1,5 @@
 package com.battleships.commands;
 
-import com.battleships.commands.values.Shot;
 import com.battleships.player.ConnectedPlayers;
 
 public class PlayersShootCommand extends AbstractCommand {
