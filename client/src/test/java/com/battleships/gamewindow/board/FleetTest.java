8c90772323b6d21a7807a5072053471fd2f91b4e
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-07-27 12:22:24 +0200

    more tests on isHit fleet method

--- client/src/test/java/com/battleships/gamewindow/board/FleetTest.java
+++ client/src/test/java/com/battleships/gamewindow/board/FleetTest.java
@@ -17,30 +17,37 @@ public class FleetTest {
     JFXPanel jfxPanel = new JFXPanel();
     private Fleet testFleet;
     private Set<BoardField> oneMastAt11;
+    private Set<BoardField> twoMastAt2526;
 
     @BeforeMethod
     public void setUp() {
         testFleet = new Fleet();
         oneMastAt11 = new HashSet<>();
         oneMastAt11.add(new BoardField(Coordinate.fromIntCoords(1, 1), FieldState.MAST));
+        twoMastAt2526 = new HashSet<>();
+        twoMastAt2526.add(new BoardField(Coordinate.fromIntCoords(2, 5), FieldState.MAST));
+        twoMastAt2526.add(new BoardField(Coordinate.fromIntCoords(2, 6), FieldState.MAST));
     }
 
     @Test
     public void shoulAddShip() {
         testFleet.addShip(new Ship(oneMastAt11));
+        testFleet.addShip(new Ship(twoMastAt2526));
 
-        assertEquals(testFleet.size(), 1);
+        assertEquals(testFleet.size(), 2);
     }
 
     @Test
     public void shouldMarkMastAsHit() {
         testFleet.addShip(new Ship(oneMastAt11));
+        testFleet.addShip(new Ship(twoMastAt2526));
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertTrue(testFleet.isHit(Coordinate.fromIntCoords(1, 1)));
         softAssert.assertFalse(testFleet.isHit(Coordinate.fromIntCoords(1, 2)));
         softAssert.assertFalse(testFleet.isHit(Coordinate.fromIntCoords(2, 2)));
         softAssert.assertFalse(testFleet.isHit(Coordinate.fromIntCoords(5, 2)));
+        softAssert.assertTrue(testFleet.isHit(Coordinate.fromIntCoords(2, 5)));
         softAssert.assertAll();
     }
 }
