2ad24439988dd04122b5aa11167647bbfca6ec14
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-07-27 12:46:34 +0200

    ship hit implemented

--- client/src/main/java/com/battleships/gamewindow/board/Ship.java
+++ client/src/main/java/com/battleships/gamewindow/board/Ship.java
@@ -2,8 +2,10 @@ package com.battleships.gamewindow.board;
 
 import com.battleships.Coordinate;
 import com.battleships.gamewindow.board.fieldStates.BoardField;
+import com.battleships.gamewindow.board.fieldStates.FieldState;
 
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 
 public class Ship {
@@ -18,14 +20,26 @@ public class Ship {
     }
 
     boolean isMastHit(Coordinate coordinate) {
-        return masts.stream().anyMatch(m -> m.getCoordinate().equals(coordinate));
+        return getMastForCoordinate(coordinate)
+                .orElseGet(() -> new BoardField(Coordinate.fromIntCoords(0, 0), FieldState.SEA))
+                .isHit();
+    }
+
+    private Optional<BoardField> getMastForCoordinate(Coordinate coordinate) {
+        return masts.stream().filter(boardField -> boardField.getCoordinate().equals(coordinate))
+                .findFirst();
     }
 
     boolean isSunk() {
         return masts.stream().allMatch(BoardField::isHit);
     }
 
-    public String toString(){
+    public String toString() {
         return masts.toString();
+    }
+
+    public void hit(Coordinate hitCoordinate) {
+        getMastForCoordinate(hitCoordinate)
+                .ifPresent(boardField -> boardField.hit());
     }
 }
--- client/src/test/java/com/battleships/gamewindow/board/ShipTest.java
+++ client/src/test/java/com/battleships/gamewindow/board/ShipTest.java
@@ -6,6 +6,7 @@ import com.battleships.gamewindow.board.fieldStates.FieldState;
 import javafx.embed.swing.JFXPanel;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,7 +19,7 @@ public class ShipTest {
     private Ship ship;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         Set<BoardField> masts = new HashSet<>();
         masts.add(new BoardField(Coordinate.fromIntCoords(1, 1), FieldState.MAST));
         masts.add(new BoardField(Coordinate.fromIntCoords(1, 2), FieldState.MAST));
@@ -34,6 +35,8 @@ public class ShipTest {
 
     @Test
     public void isMastShot_whenHitMast_shouldReturnTrue() {
+        ship.hit(Coordinate.fromIntCoords(1, 1));
+
         Coordinate itIsNotAFuckingMast = Coordinate.fromIntCoords(1, 1);
         assertTrue(ship.isMastHit(itIsNotAFuckingMast));
     }
@@ -42,5 +45,19 @@ public class ShipTest {
     public void isMastSunk_whenAllMastsHit_shouldReturnTrue() {
         ship.getMasts().forEach(BoardField::hit);
         assertTrue(ship.isSunk());
+    }
+
+    @Test
+    public void shouldSinkSelectedMast() {
+        Coordinate hitCoordinate = Coordinate.fromIntCoords(1, 2);
+
+        ship.hit(hitCoordinate);
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(ship.isMastHit(hitCoordinate));
+        softAssert.assertFalse(ship.isSunk());
+        softAssert.assertFalse(ship.isMastHit(Coordinate.fromIntCoords(1, 1)));
+        softAssert.assertFalse(ship.isMastHit(Coordinate.fromIntCoords(1, 3)));
+        softAssert.assertAll();
     }
 }
